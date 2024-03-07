1b231e61023e75583bbd34d47d6f7824f5eae063
adamr <adam.riha97@gmail.com>
2024-03-08 00:52:44 +0100

    output_new.csv with validated addresses added

--- address_validation.py
+++ address_validation.py
@@ -34,6 +34,27 @@ def validateAddress(url, address):
     else:
         print("POST request failed with status code:", response.status_code)
 
+def addAddressToDF(address):
+    global val_addr_df
+    folder_name = 'validated_addresses'
+    json_filename = os.path.join(folder_name, f'{address}.json')
+    with open(json_filename, 'r') as f:
+        json_data = json.load(f)
+    
+    new_row_data = {
+        'orig_string': address
+    }
+    new_row_df = pd.DataFrame([new_row_data])
+    val_addr_df = pd.concat([val_addr_df, new_row_df], ignore_index=True)
+
+    val_addr_df.loc[val_addr_df['orig_string'] == address, 'latitude'] = json_data['result']['geocode']['location']['latitude']
+    val_addr_df.loc[val_addr_df['orig_string'] == address, 'longitude'] = json_data['result']['geocode']['location']['longitude']
+
+    components = json_data['result']['address']['addressComponents']
+    for component in components:
+        if component['confirmationLevel'] == 'CONFIRMED':
+            val_addr_df.loc[val_addr_df['orig_string'] == address, component['componentType']] = component['componentName']['text']
+
 if __name__ == "__main__":
     # Load environment variables from .env file
     load_dotenv()
@@ -49,5 +70,14 @@ if __name__ == "__main__":
     df['address_for_validation'] = df['ulice'] + ' ' + df['psc'] + ' ' + df['mesto'] # bude nutno prepsat
     addresses = df['address_for_validation'].tolist()
     addresses = addresses[:5] # bude nutno smazat
+
+    columns = ['locality', 'route', 'street_number', 'postal_code', 'sublocality_level_1', 'neighborhood', 'country', 'latitude', 'longitude', 'orig_string']
+    val_addr_df = pd.DataFrame(columns=columns)
+
     for address in addresses:
-        response = validateAddress(url=api_url_with_api_key, address=address)
+        #response = validateAddress(url=api_url_with_api_key, address=address)
+        addAddressToDF(address)
+    
+    csv_file = 'output_new.csv'
+    val_addr_df.to_csv(csv_file, index=False)
+    print(val_addr_df)
